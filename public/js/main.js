549cf824b6ff559cb1cf063b9bf15b55a8160b57
Martin Page <wookoouk@gmail.com>
2014-12-18 16:26:18 +0000

    charts are now pretty (thanks Shyam)

--- public/js/main.js
+++ public/js/main.js
@@ -1,97 +1,172 @@
-var updateCharts = function () {
-    $('.ct-chart').each(function () {
-        var el = this;
-        var id = $(el).attr('data-monitor');
-        var count = $(el).attr('data-mcount') || 8;
-        if (id) {
-            $.getJSON('/monitors/' + id + '/status/' + count, function (data) {
-                $(el).empty();
-                if (data && data.length > 0) {
-                    var series = [];
-                    var labels = [];
-                    data.forEach(function (res) {
+var easeOutQuad = function (x, t, b, c, d) {
+    return -c * (t /= d) * (t - 2) + b;
+};
 
-                        var time = moment(res.createdAt);
-                        var now = moment();
+$(document).ready(function () {
+    var updateCharts = function () {
+        $('.ct-chart').each(function () {
+            var el = this;
+            var seq = 0;
+            var animationComplete = false;
+            var id = $(el).attr('data-monitor');
+            var count = $(el).attr('data-mcount') || 8;
+            if (id) {
+                $.getJSON('/monitors/' + id + '/status/' + count, function (data) {
+                    $(el).empty();
+                    if (data && data.length > 0) {
+                        var series = [];
+                        var labels = [];
+                        data.forEach(function (res) {
 
-                        var fancyTime = 'unknown';
+                            var time = moment(res.createdAt);
+                            var now = moment();
 
-                        if (now.year(time)) {
-                            fancyTime = time.format("DD/MM");
-                            if (now.month(time)) {
-                                fancyTime = time.format("ddd Do");
-                                if (now.week(time)) {
-                                    fancyTime = time.format("ddd");
-                                    if (now.day(time)) {
-                                        fancyTime = time.format("hh:mm");
-                                        if (now.hour(time)) {
-                                            fancyTime = time.fromNow();
+                            var fancyTime = 'unknown';
+
+                            if (now.year(time)) {
+                                fancyTime = time.format("DD/MM");
+                                if (now.month(time)) {
+                                    fancyTime = time.format("ddd Do");
+                                    if (now.week(time)) {
+                                        fancyTime = time.format("ddd");
+                                        if (now.day(time)) {
+                                            fancyTime = time.format("hh:mm");
+                                            if (now.hour(time)) {
+                                                fancyTime = time.fromNow();
+                                            }
                                         }
                                     }
                                 }
+                            } else {
+                                fancyTime = time.format("DD/MM/YY hh:mm");
                             }
-                        } else {
-                            fancyTime = time.format("DD/MM/YY");
-                        }
 
 
-                        labels.push(fancyTime);
-                        series.push(res.time);
-                    });
-                    var chart = new Chartist.Line(el, {
-                        labels: labels,
-                        series: [series]
-                    }, {showArea: true});
-                    chart.on('created', function () {
-                        setUpChartInteraction($(el));
-                    });
-                } else {
-                    $(el).append('<h3>No data yet...</h3>');
+                            labels.push(fancyTime);
+                            series.push(res.time);
+                        });
+
+                        var chart = new Chartist.Line(el, {
+                            labels: labels,
+                            series: [series]
+                        }, {showArea: true});
+
+                        chart.on('created', function () {
+                            seq = 0;
+                            animationComplete = true;
+                            setUpChartInteraction($(el));
+                        });
+
+
+                        chart.on('draw', function (data) {
+                            if (!animationComplete) {
+
+                                if (data.type === 'area') {
+                                    seq++;
+                                    data.element.animate({
+                                        opacity: {
+                                            from: 0,
+                                            to: 1,
+                                            begin: seq * 100,
+                                            dur: 400
+                                        }
+                                    });
+                                }
+                                if (data.type === 'line') {
+                                    seq++;
+                                    data.element.animate({
+                                        opacity: {
+                                            from: 0,
+                                            to: 1,
+                                            begin: seq * 80,
+                                            dur: 400
+                                        }
+                                    })
+                                }
+
+                                if (data.type === 'point') {
+                                    seq++;
+                                    data.element.animate({
+                                        y1: {
+                                            from: data.y - 50,
+                                            to: data.y,
+                                            begin: seq * 80,
+                                            dur: 400,
+                                            easing: Chartist.Svg.Easing.easeOutQuint
+                                        },
+                                        opacity: {
+                                            from: 0,
+                                            to: 1,
+                                            begin: seq * 80,
+                                            dur: 100
+                                        }
+                                    });
+                                }
+                            }
+                        });
+
+                    } else {
+                        $(el).append('<h3>No data yet...</h3>');
+                    }
+                });
+            }
+        });
+    };
+
+
+    function setUpChartInteraction($chart) {
+
+
+        {
+            var points = $chart.find('.ct-point');
+            points.each(function () {
+                var point = $(this);
+                var value = point.attr('ct:value');
+                if (value == 0) {
+                    point.css('stroke', '#E74C3C');
                 }
             });
         }
-    });
-};
-function setUpChartInteraction($chart) {
-    var easeOutQuad = function (x, t, b, c, d) {
-        return -c * (t /= d) * (t - 2) + b;
-    };
-    $chart
-        .append('<div class="chartTooltip"></div>');
-    var $toolTip = $chart.find('.chartTooltip')
-        .hide();
-    $chart.find('.ct-point').mouseenter(function () {
-        var $point = $(this);
-        var value = $point.attr('ct:value');
-        $point.animate({'stroke-width': '30px'}, 300, easeOutQuad);
-        $toolTip.html(value + 'ms').show();
-    });
-    $chart.find('.ct-point').mouseleave(function () {
-        var $point = $(this);
-        $point.animate({'stroke-width': '10px'}, 300, easeOutQuad);
-        $toolTip.hide();
-    });
-    $chart.find('.ct-point').mousemove(function (event) {
-        $toolTip.css({
-            left: (event.originalEvent.layerX) - $toolTip.width() / 2,
-            top: (event.originalEvent.layerY) - $toolTip.height() - 40
+
+
+        $chart
+            .append('<div class="chartTooltip"></div>');
+        var $toolTip = $chart.find('.chartTooltip')
+            .hide();
+        $chart.find('.ct-point').mouseenter(function () {
+            var $point = $(this);
+            var value = $point.attr('ct:value');
+            $point.animate({'stroke-width': '30px'}, 300, easeOutQuad);
+            $toolTip.html(value + 'ms').show();
         });
-    });
-}
+        $chart.find('.ct-point').mouseleave(function () {
+            var $point = $(this);
+            $point.animate({'stroke-width': '10px'}, 300, easeOutQuad);
+            $toolTip.hide();
+        });
+        $chart.find('.ct-point').mousemove(function (event) {
+            $toolTip.css({
+                left: (event.originalEvent.layerX) - $toolTip.width() / 2,
+                top: (event.originalEvent.layerY) - $toolTip.height() - 40
+            });
+        });
+    }
 
-function scrollManager() {
-    $(".navbar").headroom({
-        "offset": 50,
-        "tolerance": 5,
-        "classes": {
-            "initial": "animated",
-            "pinned": "slideDown",
-            "unpinned": "slideUp"
-        }
-    });
-}
+    function scrollManager() {
+        $(".navbar").headroom({
+            "offset": 50,
+            "tolerance": 5,
+            "classes": {
+                "initial": "animated",
+                "pinned": "slideDown",
+                "unpinned": "slideUp"
+            }
+        });
+    }
 
-$(window).bind("load", function () {
-    updateCharts();
-    scrollManager();
+    $(window).bind("load", function () {
+        updateCharts();
+        scrollManager();
+    });
+
 });

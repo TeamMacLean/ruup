b5ba211567b2233c9045b3d919b0efabf4f6be55
Martin Page <wookoouk@gmail.com>
2015-06-12 12:00:42 +0100

    add more recipients

--- controllers/monitors.js
+++ controllers/monitors.js
@@ -3,142 +3,159 @@ var util = require('../lib/util');
 
 module.exports.controller = function (app) {
 
-    app.get('/monitors', function (req, res) {
-        Monitor.find({}, function (err, monitors) {
-            if (err) {
-                return util.renderError(err, res);
-            }
-            return res.render('monitors/index', {monitors: monitors});
-        });
+  app.get('/monitors', function (req, res) {
+    Monitor.find({}, function (err, monitors) {
+      if (err) {
+        return util.renderError(err, res);
+      }
+      return res.render('monitors/index', {monitors: monitors});
     });
+  });
 
-    app.route('/monitors/add')
-        .get(isAuthenticated, function (req, res) {
-            return res.render('monitors/add');
-        })
-        .post(isAuthenticated, function (req, res) {
-            var nameInput = req.body.nameInput;
-            var urlInput = req.body.urlInput;
-            var rateInput = req.body.rateInput;
+  app.route('/monitors/add')
+    .get(isAuthenticated, function (req, res) {
+      return res.render('monitors/add');
+    })
+    .post(isAuthenticated, function (req, res) {
+      var nameInput = req.body.nameInput;
+      var urlInput = req.body.urlInput;
+      var rateInput = req.body.rateInput;
 
-            var currentUser = req.user;
+      var currentUser = req.user;
 
 
-            if (nameInput && urlInput && rateInput && currentUser) {
-                var userID = currentUser._id;
-                var monitor = new Monitor({
-                    name: nameInput,
-                    url: urlInput,
-                    rate: rateInput,
-                    owner: userID
-                });
-                monitor.save(function (err, gen) {
-                    if (err) {
-                        return util.renderError(err, res);
-                    } else {
-                        gen.start();
-                        return res.redirect('/monitors');
-                    }
-                });
-            } else {
-                return util.renderError('bad input', res);
-            }
+      if (nameInput && urlInput && rateInput && currentUser) {
+        var userID = currentUser._id;
+        var monitor = new Monitor({
+          name: nameInput,
+          url: urlInput,
+          rate: rateInput,
+          owner: userID
         });
-
-    app.get('/monitors/:id', function (req, res) {
-        Monitor.findOne({_id: req.param('id')}, function (err, monitor) {
-            if (err) {
-                return util.renderError(err, res);
-            }
-
-            monitor.getEvents(function (err, events) {
-
-                if (err) {
-                    return util.renderError(err, res);
-                }
-
-                return res.render('monitors/show', {monitor: monitor, events: events});
-            });
-
-
-        });
-    });
-
-    app.get('/monitors/:id/status/:count', function (req, res) {
-        Monitor.findOne({_id: req.param('id')}, function (err, monitor) {
-            if (err) {
-                return util.renderError(err, res);
-            }
-            var count = req.param('count');
-            monitor.getResponses(count, function (err, responses) {
-                if (err) {
-                    return util.renderError(err, res);
-                }
-                return res.send(responses.reverse());
-            });
-
-        });
-    });
-
-    app.route('/monitors/:id/delete')
-        .get([isAuthenticated, isOwner], function (req, res) {
-            var id = req.param('id');
-            Monitor.findOne({_id: id}).exec(function (err, doc) {
-                if (err) {
-                    return util.renderError(err, res);
-                }
-                return res.render('monitors/confirmDelete', {monitor: doc});
-            });
-        })
-        .post([isAuthenticated, isOwner], function (req, res) {
-            var id = req.param('id');
-            Monitor.findOne({_id: id}).exec(function (err, doc) {
-                if (err) {
-                    return util.renderError(err, res);
-                }
-
-                doc.removeEvents(function (err) {
-                    if (err) {
-                        return util.renderError(err, res);
-                    }
-
-                    doc.removeResponses(function (err) {
-                        if (err) {
-                            return util.renderError(err, res);
-                        }
-                        doc.remove(function (err) {
-                            if (err) {
-                                return util.renderError(err, res);
-                            }
-                            return res.redirect('/monitors');
-                        })
-                    })
-                });
-            });
-        });
-    function isAuthenticated(req, res, next) {
-        if (req.isUnauthenticated()) {
-            return res.redirect('/signin');
-        }
-        return next();
-    }
-
-    function isOwner(req, res, next) {
-        var id = req.param('id');
-        if (id) {
-            Monitor.findOne({_id: id}, function (err, doc) {
-                if (err) {
-                    return util.renderError(err, res);
-                }
-                if (req.isAuthenticated() && req.user._id == doc.owner) {
-                    return next();
-                } else {
-                    return util.renderError('you do not own that monitor', res);
-                }
-            });
-        } else {
+        monitor.save(function (err, gen) {
+          if (err) {
+            return util.renderError(err, res);
+          } else {
+            gen.start();
             return res.redirect('/monitors');
+          }
+        });
+      } else {
+        return util.renderError('bad input', res);
+      }
+    });
+
+  app.get('/monitors/:id', function (req, res) {
+    Monitor.findOne({_id: req.param('id')}, function (err, monitor) {
+      if (err) {
+        return util.renderError(err, res);
+      }
+
+      monitor.getEvents(function (err, events) {
+
+        if (err) {
+          return util.renderError(err, res);
         }
+
+        return res.render('monitors/show', {monitor: monitor, events: events});
+      });
+
+
+    });
+  });
+
+  app.get('/monitors/:id/status/:count', function (req, res) {
+    Monitor.findOne({_id: req.param('id')}, function (err, monitor) {
+      if (err) {
+        return util.renderError(err, res);
+      }
+      var count = req.param('count');
+      monitor.getResponses(count, function (err, responses) {
+        if (err) {
+          return util.renderError(err, res);
+        }
+        return res.send(responses.reverse());
+      });
+
+    });
+  });
+
+  app.route('/monitors/:id/delete')
+    .get([isAuthenticated, isOwner], function (req, res) {
+      var id = req.param('id');
+      Monitor.findOne({_id: id}).exec(function (err, doc) {
+        if (err) {
+          return util.renderError(err, res);
+        }
+        return res.render('monitors/confirmDelete', {monitor: doc});
+      });
+    })
+    .post([isAuthenticated, isOwner], function (req, res) {
+      var id = req.param('id');
+      Monitor.findOne({_id: id}).exec(function (err, doc) {
+        if (err) {
+          return util.renderError(err, res);
+        }
+
+        doc.removeEvents(function (err) {
+          if (err) {
+            return util.renderError(err, res);
+          }
+
+          doc.removeResponses(function (err) {
+            if (err) {
+              return util.renderError(err, res);
+            }
+            doc.remove(function (err) {
+              if (err) {
+                return util.renderError(err, res);
+              }
+              return res.redirect('/monitors');
+            })
+          })
+        });
+      });
+    });
+
+  app.route('/monitors/:id/addEmail')
+    .post([isAuthenticated, isOwner], function (req, res) {
+      var id = req.param('id');
+      var newEmail = req.body.addEmail;
+
+      Monitor.findOne({_id: id}).exec(function (err, doc) {
+        if (err) {
+          return util.renderError(err, res);
+        } else {
+          doc.otherObservers.push(newEmail);
+          return res.redirect('/monitors/' + id);
+        }
+
+      })
+    });
+
+  function isAuthenticated(req, res, next) {
+    if (req.isUnauthenticated()) {
+      return res.redirect('/signin');
     }
+    return next();
+  }
+
+  function isOwner(req, res, next) {
+    var id = req.param('id');
+    if (id) {
+      Monitor.findOne({_id: id}, function (err, doc) {
+        if (err) {
+          return util.renderError(err, res);
+        }
+        if (req.isAuthenticated() && req.user._id == doc.owner) {
+          return next();
+        } else {
+          return util.renderError('you do not own that monitor', res);
+        }
+      });
+    } else {
+      return res.redirect('/monitors');
+    }
+  }
 
 };
--- models/monitor.js
+++ models/monitor.js
@@ -7,130 +7,130 @@ var path = require('path');
 var event = require('./event');
 
 var monitorScheme = mongoose.Schema({
-    name: {type: String, required: true},
-    url: {type: String, required: true},
-    rate: {type: Number, required: true, min: 5, max: 1440},
-    owner: {type: String, required: true},
-    downNoticed: Boolean,
-    downNotified: Boolean,
-    createdAt: Date
-
+  name: {type: String, required: true},
+  url: {type: String, required: true},
+  rate: {type: Number, required: true, min: 5, max: 1440},
+  owner: {type: String, required: true},
+  downNoticed: Boolean,
+  downNotified: Boolean,
+  createdAt: Date,
+  otherObservers: [String]
 });
 
 function saveChanges(model) {
-    model.save(model, function (err) {
-        util.logInfo('error saving model', model, err);
-    })
+  model.save(model, function (err) {
+    util.logInfo('error saving model', model, err);
+  })
 }
 
 function isDown(err, response) {
-    if (err) {
-        return true;
-    }
-    return response.statusCode != 200;
+  if (err) {
+    return true;
+  }
+  return response.statusCode != 200;
 
 }
 
 function processDown(monitor) {
 
-    if (monitor.downNoticed) { //it was down at last check
-        if (!monitor.downNotified) {
-            monitor.downNotified = true;
-            saveChanges(monitor);
-            var downEvent = new event({monitor: monitor._id, time: Date.now(), type: event.types.down});
-            saveChanges(downEvent);
-            email.notifyDown(monitor);
-        }
-    } else {
-        monitor.downNoticed = true; // next time I will send out an email
-        saveChanges(monitor);
+  if (monitor.downNoticed) { //it was down at last check
+    if (!monitor.downNotified) {
+      monitor.downNotified = true;
+      saveChanges(monitor);
+      var downEvent = new event({monitor: monitor._id, time: Date.now(), type: event.types.down});
+      saveChanges(downEvent);
+      email.notifyDown(monitor);
     }
+  } else {
+    monitor.downNoticed = true; // next time I will send out an email
+    saveChanges(monitor);
+  }
 }
 
 function processUp(monitor) {
 
-    if (monitor.downNoticed) {
-        monitor.downNoticed = false;
-        saveChanges(monitor);
-        if (monitor.downNotified) {
-            monitor.downNotified = false;
-            saveChanges(monitor);
+  if (monitor.downNoticed) {
+    monitor.downNoticed = false;
+    saveChanges(monitor);
+    if (monitor.downNotified) {
+      monitor.downNotified = false;
+      saveChanges(monitor);
 
-            var upEvent = new event({monitor: monitor._id, time: Date.now(), type: event.types.up});
-            saveChanges(upEvent);
-            email.notifyUp(monitor);
-        }
+      var upEvent = new event({monitor: monitor._id, time: Date.now(), type: event.types.up});
+      saveChanges(upEvent);
+      email.notifyUp(monitor);
     }
+  }
 }
 
 monitorScheme.methods.curl = function () {
-    var monitor = this;
-    var id = monitor._id;
-    var then = new Date().getTime();
+  var monitor = this;
+  var id = monitor._id;
+  var then = new Date().getTime();
 
-    request(this.url, function (err, response) {
+  request(this.url, function (err, response) {
 
-        var now = new Date().getTime();
-        var time = now - then;
+    var now = new Date().getTime();
+    var time = now - then;
 
-        if (isDown(err, response)) {
-            processDown(monitor);
-        } else {
-            processUp(monitor);
-        }
+    if (isDown(err, response)) {
+      processDown(monitor);
+    } else {
+      processUp(monitor);
+    }
 
-        var code = 'error';
-        if (err) {
-            time = 0;
-        } else {
-            code = response.statusCode
-        }
+    var code = 'error';
+    if (err) {
+      time = 0;
+    } else {
+      code = response.statusCode
+    }
 
-        makeResponse(err, code, time, id)
-    });
+    makeResponse(err, code, time, id)
+  });
 };
 
 var makeResponse = function (err, code, time, monitor) {
-    util.logInfo(time + 'ms');
-    var responseBack = new response({
-        code: code,
-        time: time,
-        monitor: monitor
-    });
-    responseBack.save(function (err) {
-        if (err) {
-            util.logError('ERROR', err);
-        }
-    });
+  util.logInfo(time + 'ms');
+  var responseBack = new response({
+    code: code,
+    time: time,
+    monitor: monitor
+  });
+  responseBack.save(function (err) {
+    if (err) {
+      util.logError('ERROR', err);
+    }
+  });
 };
 
 monitorScheme.methods.start = function () {
-    var monitor = this;
-    util.logInfo('started monitoring', monitor.name);
-    setInterval(function () {
-        monitor.curl();
-    }, this.rate * 60 * 1000);
+  var monitor = this;
+  util.logInfo('started monitoring', monitor.name);
+  setInterval(function () {
+    monitor.curl();
+  }, this.rate * 60 * 1000);
 };
 
 monitorScheme.methods.getEvents = function (cb) {
-    var monitor = this;
-    event.find({monitor: monitor._id}).exec(cb);
+  var monitor = this;
+  event.find({monitor: monitor._id}).exec(cb);
 
 };
 
 monitorScheme.methods.getResponses = function (count, cb) {
-    var monitor = this;
-    response.find({monitor: monitor._id}).sort({createdAt: 'desc'}).limit(count).exec(cb);
+  var monitor = this;
+  response.find({monitor: monitor._id}).sort({createdAt: 'desc'}).limit(count).exec(cb);
 };
 
 monitorScheme.methods.removeResponses = function (cb) {
-    var monitor = this;
-    response.remove({monitor: monitor._id}).exec(cb);
+  var monitor = this;
+  response.remove({monitor: monitor._id}).exec(cb);
 };
 
-monitorScheme.methods.removeEvents = function(cb){
-    var monitor = this;
-    event.find({monitor: monitor._id}).remove(cb);
+monitorScheme.methods.removeEvents = function (cb) {
+  var monitor = this;
+  event.find({monitor: monitor._id}).remove(cb);
 }
 
 var Monitor = mongoose.model('Monitor', monitorScheme);

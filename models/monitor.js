b04559e78b018ce0418c9dd7bfce6623746275db
Martin Page <wookoouk@gmail.com>
2016-09-27 11:12:35 +0100

    fix value names

--- models/monitor.js
+++ models/monitor.js
@@ -1,21 +1,13 @@
 const thinky = require('../lib/thinky');
 const type = thinky.type;
-// const safeName = require('../lib/safeName');
-
 const badge = require('../lib/badge');
 
 const Monitor = thinky.createModel('Monitor', {
     id: type.string(),
     name: type.string().required(),
-    // safeName: type.string().required(),
     url: type.string().required(),
     username: type.string().required()
 });
-
-// Monitor.pre('save', function (next) {
-//     this.safeName = safeName(this.name);
-//     next();
-// });
 
 Monitor.define('getUpPercent', function () {
     return 100;

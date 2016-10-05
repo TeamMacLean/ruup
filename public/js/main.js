d1ecd69befd473ea1bb7f4576402698de6859ab1
Martin Page <wookoouk@gmail.com>
2016-10-05 10:51:50 +0100

    test

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,9 @@
-$(function () {
-    var overlay = $('.md-overlay');
-    var modal = $('.md-modal');
+// $(function () {
+var overlay = $('.md-overlay');
+var modal = $('.md-modal');
 
-    $('.md-trigger').on('click', function () {
-        modal.toggleClass('md-show');
-        overlay.toggleClass('md-show');
-    })
-
-});
+$('.md-trigger').on('click', function () {
+    modal.toggleClass('md-show');
+    overlay.toggleClass('md-show');
+})
+// });
